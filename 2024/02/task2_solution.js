3a0f084deb0a9edd37edc5d7d754f9d7bc878f30
Rafał Ciećwierz <rafal.ciecwierz4@gmail.com>
2025-01-04 20:33:57 +0100

    add: task2, part 2 solution

--- 2024/02/task2_solution.js
+++ 2024/02/task2_solution.js
@@ -10,7 +10,7 @@ const rows = input.split('\n')
 // calculate the safe levels
 const result = rows.reduce((acc, row) => {
     const level = rowToNumber(row);
-    return isLevelSafe(level) && acc + 1;
+    return isLevelSafe(level) ? acc + 1 : acc;
 }, 0);
 
 console.log('PART 1====================================');
@@ -38,45 +38,26 @@ function isLevelSafe(row) {
 
 // Part 2
 function isLevelSafeWithProblemDumper(row) {
-    if (isLevelSafe(row)) return true;
-
-    console.log(row);
-
-    return row.some((_, index) => {
-        const newRow = [...row];
-        newRow.splice(index, 1);
-
-        if (isLevelSafe(newRow)) {
-            return true;
-        } else {
-            // console.log(newRow);
-        }
-    });
-}
-
-// console.log(isLevelSafeWithProblemDumper([9, 11, 12, 16, 13]));
-
-// const resultPart2 = rows.reduce((acc, row) => {
-//     const level = rowToNumber(row);
-//     return isLevelSafeWithProblemDumper(level) && acc + 1;
-// }, 0);
-
-console.log('PART 2====================================');
-// console.log(resultPart2);
-console.log('====================================');
-
-const test = [9, 11, 12, 16, 13, 11];
-
-console.log(test.some((_, index) => {
-    const newRow = [...test];
-
-
-    newRow.splice(index, 1);
-    console.log(newRow)
-
-    if (isLevelSafe(newRow)) {
+    if (isLevelSafe(row)) {
         return true;
     } else {
-        // console.log(newRow);
+        return row.some((_, index) => {
+            const newRow = [...row];
+            newRow.splice(index, 1);
+
+            if (isLevelSafe(newRow)) {
+                return true;
+            }
+        });
     }
-}));
+}
+
+const resultPart2 = rows.reduce((acc, row) => {
+    const level = rowToNumber(row);
+    return isLevelSafeWithProblemDumper(level) ? acc + 1 : acc;
+}, 0);
+
+console.log('PART 2====================================');
+console.log(resultPart2);
+console.log('====================================');
+
